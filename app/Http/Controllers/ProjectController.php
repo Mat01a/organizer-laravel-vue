58ac02cf7155b45d4cc6a5e4122ae10a96c300d8
Mat Ol <matolszakowski@gmail.com>
2023-09-04 22:43:45 +0200

    Added: - Adding users to the project. - Proposed users adding users. - Showing current users in project. Modified: - Changed registering users/ added new username column. - Seed factory to new restrictions. - Function name for better name convenction. Fixed: - Properly showing for each users their projects.

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Permission;
 use App\Models\Project;
+use App\Models\User;
 
 class ProjectController extends Controller
 {
@@ -37,7 +38,14 @@ class ProjectController extends Controller
         DB::beginTransaction();
 
         try {
-            $last_project_id = Project::select('id')->get()->last()->id;
+            $number_of_projects = Project::select('id')->count();
+            if($number_of_projects > 0)
+            {
+                $last_project_id = Project::select('id')->get()->last()->id;
+            }
+            else {
+                $last_project_id = 0;
+            }
 
 
             //Creating project
@@ -65,7 +73,7 @@ class ProjectController extends Controller
         //return response(200, "New project has been created");
     }
 
-    public function project_difference($column, $data, $operator = '=')
+    public function projectDifference($column, $data, $operator = '=')
     {
         $difference = Project::where($column, $operator, $data)->get();
 
@@ -74,6 +82,7 @@ class ProjectController extends Controller
     }
 
 
+    // Changing project name
     public function update(Request $request)
     {
         $user = $request->user();
@@ -91,7 +100,7 @@ class ProjectController extends Controller
 
 
         // Changes name if there's difference between current one
-        if($permissions_project && !$this->project_difference('name', $changed_name))
+        if($permissions_project && !$this->projectDifference('name', $changed_name))
         {
             try {
                 # Updating name
@@ -106,4 +115,92 @@ class ProjectController extends Controller
 
     }
 
+
+    public function findProposedUsers($id, $username)
+    {
+        $users_projects = Permission::select('user_id')
+        ->where('project_id', '=', $id)->get();
+        
+        $search_username = $username.'%';
+        $data = User::select('name','username')
+        ->where('username', 'like', $search_username)
+        ->whereNotIn('id', $users_projects)
+        ->limit(5)
+        ->get();
+        return response($data, 200);
+    }
+
+
+    // Add user
+    public function addUser(Request $request)
+    {
+        $current_user = $request->user();
+        try {
+
+            $requested_user = User::where('username', '=', $request->username)->first();
+            if($requested_user)
+            {
+                $requested_user_permissions = Permission::where('user_id', '=', $requested_user->id)->get();
+            }
+            else {
+                return response("There is no a user with that username.", 400);
+            }
+
+            foreach ($requested_user_permissions as $key => $value) {
+                # code...
+                if($value->project_id === $request->id)
+                {
+                    return response("User is in current project.", 400);
+                }
+            }            
+
+            $requested_username = $request->username;
+            $user_to_be_added = User::where('username', '=',  $requested_username)
+            ->get();
+            DB::beginTransaction();
+            try {
+                $permissions = new Permission;
+
+                //giving regular permissions
+                $permissions->user_id = $user_to_be_added[0]->id;
+                $permissions->project_id = $request->id;
+                $permissions->name = 'regular';
+                $permissions->read = 0;
+                $permissions->write = 0;
+                $permissions->givePermissions = 0;
+                $permissions->save();
+                DB::commit();
+                return response()->json(['success' => true, 'name' => $user_to_be_added[0]->name, 'username' => $user_to_be_added[0]->username]);
+            } catch (\Throwable $th) {
+                throw $th;
+                error_log($th);
+                DB::rollBack();
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+            error_log($th);
+        }
+    }
+    
+
+    public function getUsersInProject($id)
+    {
+        try {
+            $permissions_users = Permission::select("user_id", "name")
+                ->where('project_id', '=', $id)
+                ->paginate(4);
+            $users = [];
+            
+            foreach ($permissions_users as $key => $value) {
+                $users[$key] = User::select("name", "username")
+                    ->where('id', '=', $value->user_id)
+                    ->get()[0];
+            }
+           return response([$users, $permissions_users], 200);
+
+        } catch (\Throwable $th) {
+            throw $th;
+            return response($th);
+        }
+    }
 }
